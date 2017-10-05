6c8198cce498219695676c50586d6ea63ca51fcd
GevinHartanto <gevinhartanto@gmail.com>
2017-10-05 21:43:13 +0700

    Retrieving Authenticated User Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()){
+		return "the user is logged in";
+	}
+	
+	//return view('welcome');
 });
 
 Route::auth();
